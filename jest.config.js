4fd75674e649bafe3c58d5f2699d35c8066858bd
Amir Asghary <itsAmirAsghary@gmail.com>
2022-06-17 01:01:33 +0430

    enabled jest to load externalResources

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
     testEnvironment: 'jsdom',
     reporters: ["default", "jest-junit"],
     setupFilesAfterEnv: ['<rootDir>/test/testSetup.js'],
+    testEnvironmentOptions: { "resources": "usable" }
 };
